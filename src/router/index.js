9381efc7d0b38459bbde6af7dd35f137de5c21ea
zhouqizhi <zhouqz@yuanqitech.com>
2023-06-26 09:31:11 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,126 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '@/view/Home.vue'
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home,
+      meta: {
+        index: 1
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/view/Login.vue'),
+      meta: {
+        index: 1
+      }
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: () => import('@/view/User.vue'),
+      meta: {
+        index: 1
+      }
+    },
+    {
+      path: '/product-list',
+      name: 'product-list',
+      component: () => import('@/view/ProductList.vue'),
+      meta: {
+        index: 2
+      }
+    },
+    {
+      path: '/category',
+      name: 'category',
+      component: () => import('@/view/Category.vue'),
+      meta: {
+        index: 1
+      }
+    },
+    {
+      path: '/product/:id',
+      name: 'product',
+      component: () => import('@/view/ProductDetail.vue'),
+      meta: {
+        index: 3
+      }
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('@/view/Cart.vue'),
+      meta: {
+        index: 1
+      }
+    },
+    {
+      path: '/create-order',
+      name: 'create-order',
+      component: () => import('@/view/CreateOrder.vue'),
+      meta: {
+        index: 2
+      }
+    },
+    {
+      path: '/address',
+      name: 'address',
+      component: () => import('@/view/Address.vue'),
+      meta: {
+        index: 2
+      }
+    },
+    {
+      path: '/address-edit',
+      name: 'address-edit',
+      component: () => import('@/view/AddressEdit.vue'),
+      meta: {
+        index: 3
+      }
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: () => import('@/view/Order.vue'),
+      meta: {
+        index: 2
+      }
+    },
+    {
+      path: '/order-detail',
+      name: 'order-detail',
+      component: () => import('@/view/OrderDetail.vue'),
+      meta: {
+        index: 3
+      }
+    },
+    {
+      path: '/setting',
+      name: 'setting',
+      component: () => import('@/view/Setting.vue'),
+      meta: {
+        index: 2
+      }
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('@/view/About.vue'),
+      meta: {
+        index: 2
+      }
+    },
+  ]
+});
+
+export default router
